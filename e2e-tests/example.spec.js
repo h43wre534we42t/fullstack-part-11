ab24707e05443d3472466f4176af437d8a8408ee
Timur <tmandziev@gmail.com>
2026-08-02 14:41:17 +0300

    ex9 final

--- e2e-tests/example.spec.js
+++ e2e-tests/example.spec.js
@@ -10,4 +10,11 @@ describe('Pokedex', () => {
       ),
     ).toBeVisible()
   })
+
+  test('a single pokemons page can be opened', async ({ page }) => {
+    await page.goto('')
+    await page.getByText('charizard').click()
+
+    await expect(page.getByText('solar power')).toBeVisible()
+  })
 })
