02cff946db077c96fbc4461599d8332cb3df68b8
Timur <tmandziev@gmail.com>
2026-08-03 16:56:14 +0300

    ex 20 breakdown simulation

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5001
 app.use(express.static('dist'))
 
 app.get('/health', (req, res) => {
-  res.send('ok')
+  res.status(500).send('Database unavailable')
 })
 
 const start = async () => {
